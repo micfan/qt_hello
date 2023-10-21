5f13c71a81a425f330e2b274bb075d3168ded4c0
Michael <2443027+micfan@users.noreply.github.com>
2023-10-22 01:18:33 +0800

    QtHello.exe: qt + cmake + vcpkg on windows

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿// CMakeProject2.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+#include <QCoreApplication>
+#include <QApplication>
+#include <QDebug>
+#include <QWidget>
+#include <QPushButton>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// cout << "Hello world" << endl;
+	QApplication app(argc, argv);
+
+	// qDebug() << "hello world";
+
+	QWidget window;
+	window.setWindowTitle("Hello world !");
+	window.show();
+
+	QPushButton button("Hello world !");
+	button.setParent(&window);
+	button.show();
+
+	return app.exec();
+}
